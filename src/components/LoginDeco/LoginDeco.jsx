61bed0d8cc4104f33979b96e71a50cbad7696002
yeo99 <qw6375@gmail.com>
2023-08-14 00:44:53 +0900

    [ADD] LoginDeco 컴포넌트 추가

---
+++ src/components/LoginDeco/LoginDeco.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './LoginDeco.scss'
+
+function LoginDeco() {
+  return (
+    <div className='login-deco-container'>
+        <div className='login-deco-div'></div>
+    </div>
+  )
+}
+
+export default LoginDeco
