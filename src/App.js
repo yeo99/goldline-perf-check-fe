645bec6412ceb6dc15017d9e37599436a31a538d
yeo99 <qw6375@gmail.com>
2023-08-22 22:07:36 +0900

    [ADD] 라우팅 가드 HOC 추가

--- src/App.js
+++ src/App.js
@@ -7,14 +7,18 @@ import Login from "./pages/LoginPage/Login";
 import Register from "./pages/RegisterPage/Register"
 import Main from './pages/MainPage/Main';
 
+// HOC
+import PrivateRoute from './components/hoc/PrivateRoute';
+
 function App() {
   return (
     <div className="App">
       <Reset/>
       <Routes>
+        <Route path="/" element={<Login/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
-        <Route path='/main' element={<Main/>}/>
+        <Route path='/main' element={<PrivateRoute><Main/></PrivateRoute>}/>
       </Routes>
     </div>
   );
