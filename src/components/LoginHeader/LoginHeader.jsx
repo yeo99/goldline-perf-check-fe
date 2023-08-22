92fd134dc3ab4cc78263eba83dbf69223d5875d5
yeo99 <qw6375@gmail.com>
2023-08-23 00:47:00 +0900

    [ADD] 로그아웃 API 연동

--- src/components/LoginHeader/LoginHeader.jsx
+++ src/components/LoginHeader/LoginHeader.jsx
@@ -1,26 +1,32 @@
 import React from 'react'
 import './LoginHeader.scss'
-import { getRailwaySection } from '../../api/railway-info/getRailway'
+import { createAxios } from '../../api/axiosInstance'
+import { useNavigate } from 'react-router-dom'
 
 const LoginHeader = () => {
-  return (
-    <div className='login-header-container'>
-        <div className='login-header-menu-wrap'>
-            <a className='login-header-logout'>로그아웃</a>
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        try {
+            const response = await createAxios.get('/logout');
+
+            if (response.status === 200) {
+                alert(response.data.message);
+                navigate('/login')
+            }
+        } catch (error) {
+            console.error('Logout failed: ', error)
+            alert('로그아웃에 실패하였습니다. 다시 시도해주세요')
+        }
+    }
+
+    return (
+        <div className='login-header-container'>
+            <div className='login-header-menu-wrap'>
+                <a className='login-header-logout' onClick={handleLogout}>로그아웃</a>
+            </div>
         </div>
-        <button onClick={ RailwaySection }>
-            버튼
-        </button>
-    </div>
-  )
+    )
 }
 
-const RailwaySection = async () => {
-    try {
-        const result = await getRailwaySection()
-        console.log(result)
-    } catch (error) {
-        alert(error.message)
-    }
-}
 export default LoginHeader
