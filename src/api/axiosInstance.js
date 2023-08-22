8aaf19ef4f1e470c8bdc983dbbeaf17c94966bac
yeo99 <qw6375@gmail.com>
2023-08-22 20:27:33 +0900

    [ADD] axios instance에 withCredentials:true 추가(CORS)

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const createAxios = axios.create({
     baseURL: 'http://localhost:3000',
     timeout: 10000,
+    withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
     }
