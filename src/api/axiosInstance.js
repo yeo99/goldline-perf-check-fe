84819f16c79d95e8545dd548398406358784ca8e
yeo99 <qw6375@gmail.com>
2023-08-23 22:22:04 +0900

    [ADD] cross-env 설정

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const createAxios = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: process.env.REACT_APP_BASE_URL,
     timeout: 10000,
     withCredentials: true,
     headers: {
