23a5c36ca509633d766c4cad891e7e310fb20669
yeo99 <qw6375@gmail.com>
2023-08-23 21:19:16 +0900

    [ADD] 평가 작성 페이지 분류 API 연동

--- src/pages/CheckPage/Check.jsx
+++ src/pages/CheckPage/Check.jsx
@@ -8,7 +8,8 @@ import Form from 'react-bootstrap/Form';
 import './Check.scss'
 
 function Check() {
-	const [answerSheet, setAnswerSheet] = useState(null);
+	const [question, setQuestion] = useState(null);
+
 	const [highClassCategory, setHighClassCategory] = useState([]);
 	const [selectedHighClass, setSelectedHighClass] = useState(null);
 
@@ -18,24 +19,60 @@ function Check() {
 	const [lowClassCategory, setLowClassCategory] = useState([]);
 	const [selectedLowClass, setSelectedLowClass] = useState([]);
 
+	const [lowestClassCategory, setLowestClassCategory] = useState([]);
+	const [selectedLowestClass, setSelectedLowestClass] = useState([]);
+
+	// 대분류 선택시 중분류 API 요청
 	const handleHighClassChange = (e) => {
 		const selectedValue = e.target.value;
 		setSelectedHighClass(selectedValue);
 
 		createAxios.get(`/facilities/middle-class/${selectedValue}`)
 			.then(response => {
-				setMiddleClassCategory(response.data)
+				if (response.data && response.data.length > 0) {
+					setMiddleClassCategory(response.data)
+				} else {
+					fetchQuestionAsCategory(selectedValue)
+				}
 			})
 	}
 
+	// 중분류 선택시 소분류 API 요청
 	const handleMiddleClassChange = (e) => {
 		const selectedValue = e.target.value;
 		setSelectedMiddleClass(selectedValue);
 
 		createAxios.get(`/facilities/low-class/${selectedValue}`)
 			.then(response => {
-				setLowClassCategory(response.data)
+				if (response.data && response.data.length > 0) {
+					setLowClassCategory(response.data)
+				} else {
+					fetchQuestionAsCategory(selectedValue)
+				}
 			})
+	}
+	
+	// 소분류 선택시 세분류 API 요청
+	const handleLowClassChange = (e) => {
+		const selectedValue = e.target.value;
+		setSelectedLowClass(selectedValue);
+
+		createAxios.get(`/facilities/lowest-class/${selectedValue}`)
+			.then(response => {
+				if (response.data && response.data.length > 0) {
+					setLowestClassCategory(response.data)
+					fetchQuestionAsCategory(selectedValue)
+				}
+			})
+	}
+
+	// 검사지를 가져옴
+	const fetchQuestionAsCategory = async (category) => {
+		try {
+			
+		} catch(error) {
+
+		}
 	}
 
 	useEffect(() => {
@@ -60,6 +97,7 @@ function Check() {
 						<div className='select-wrap'>
 							<div className='category-title'>대분류</div>
 							<Form.Select onChange={handleHighClassChange}>
+								<option value="" disabled selected>대분류를 선택해주세요</option>
 								{highClassCategory.map(item => (
 									<option key={item.id} value={item.id}>
 										{item.facility_code}
@@ -68,30 +106,47 @@ function Check() {
 							</Form.Select>
 						</div>
 
-						<div className='select-wrap'>
-							<div className='category-title'>중분류</div>
-							{/* <Form.Select onChange={handleMiddleClassChange} disabled={!selectedHighClass}> */}
-							<Form.Select onChange={handleMiddleClassChange}>
-								{!selectedHighClass && <option>대분류를 먼저 선택해주세요</option>}
-								{middleClassCategory.map(item => (
-									<option key={item.id} value={item.id}>
-										{item.facility_code}
-									</option>
-								))}
-							</Form.Select>
-						</div>
+						{middleClassCategory.length > 0 && (
+							<div className='select-wrap'>
+								<div className='category-title'>중분류</div>
+								<Form.Select onChange={handleMiddleClassChange}>
+									<option value="" disabled selected>중분류를 선택해주세요</option>
+									{middleClassCategory.map(item => (
+										<option key={item.id} value={item.id}>
+											{item.facility_code}
+										</option>
+									))}
+								</Form.Select>
+							</div>
+						)}
 
-						<div className='select-wrap'>
-							<div className='category-title'>소분류</div>
-							<Form.Select disabled={!selectedMiddleClass}>
-								{!selectedMiddleClass && <option>중분류를 먼저 선택해주세요</option>}
-								{lowClassCategory.map(item => (
-									<option key={item.id} value={item.id}>
-										{item.facility_code}
-									</option>
-								))}
-							</Form.Select>
-						</div>
+						{lowClassCategory.length > 0 && (
+							<div className='select-wrap'>
+								<div className='category-title'>소분류</div>
+								<Form.Select onChange={handleLowClassChange}>
+									<option value="" disabled selected>소분류를 선택해주세요</option>
+									{lowClassCategory.map(item => (
+										<option key={item.id} value={item.id}>
+											{item.facility_code}
+										</option>
+									))}
+								</Form.Select>
+							</div>
+						)}
+
+						{lowestClassCategory.length > 0 && (
+							<div className='select-wrap'>
+								<div className='category-title'>세분류</div>
+								<Form.Select>
+									<option value="" disabled selected>세분류를 선택해주세요</option>
+									{lowestClassCategory.map(item => (
+										<option key={item.id} value={item.id}>
+											{item.facility_code}
+										</option>
+									))}
+								</Form.Select>
+							</div>
+						)}
 					</div>
 
 				</div>
